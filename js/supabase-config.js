6c4cdddaaed2ddcc14949a6d9a8b5e4b24d2d89c
YemiGeo <yemileye2001@yahoo.com>
2026-08-21 00:31:25 +0200

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL =
-    "https://tosohfqdcagidppgohoz.supabase.co/rest/v1/";
+    "https://tosohfqdcagidppgohoz.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
     "sb_publishable_LY5rEEgMekxuxCeLrqR7cQ_8FwXn1Io";
